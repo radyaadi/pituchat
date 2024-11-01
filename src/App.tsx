4669de4bf74e886addb61ee6221c7069bdb98596
radyaadi <adiradya140@gmail.com>
2024-11-01 11:56:04 +0700

    feat: installing chakra ui

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,11 @@
 import "./styles/App.css";
+import { Alert } from "./components/ui/alert";
 
 function App() {
   return (
     <div className="App">
       <h1 className="text-3xl font-bold underline">Hello world!</h1>
+      <Alert title="Alert Title">This is the alert description</Alert>
     </div>
   );
 }
