7f753051f9de8b07b90f728a12e0d84f0feb70a2
radyaadi <adiradya140@gmail.com>
2024-11-02 08:05:53 +0700

    feat(router): set up initial routing configuration

--- src/index.tsx
+++ src/index.tsx
@@ -1,17 +1,45 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import "./styles/index.css";
-import App from "./App";
+import { createBrowserRouter, Outlet, RouterProvider } from "react-router-dom";
 import reportWebVitals from "./reportWebVitals";
 import { Provider } from "./components/ui/provider";
+import App from "./App";
+import "./styles/index.css";
+
+const router = createBrowserRouter([
+  {
+    path: "/login",
+    element: <App />,
+  },
+  {
+    path: "/",
+    element: (
+      <div>
+        <p>Main Content Layout</p>
+        <Outlet />
+      </div>
+    ),
+    // children: [
+    //   {
+    //     path: "chat",
+    //     element: <p>Chat Layout</p>,
+    //   },
+    //   {
+    //     path: "toko",
+    //     element: <p>Toko Layout</p>,
+    //   },
+    // ],
+  },
+]);
 
 const root = ReactDOM.createRoot(
   document.getElementById("root") as HTMLElement,
 );
+
 root.render(
   <React.StrictMode>
     <Provider>
-      <App />
+      <RouterProvider router={router} />
     </Provider>
   </React.StrictMode>,
 );
